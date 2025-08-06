3f494ee53c1fd7c175362e34c57337319f9c43a9
Filippo Menchini <filippo.menchini99@gmail.com>
2025-08-06 10:53:55 +0200

    basic tee implementation

--- 4.10_4-1_tee.zig
+++ 4.10_4-1_tee.zig
@@ -1,11 +1,36 @@
 const std = @import("std");
-const args = @import("args.zig");
+
+const BUFFER_SIZE = 4096;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    var buffer: [BUFFER_SIZE]u8 = undefined;
 
-    const arguments = try args.get(allocator);
-    defer allocator.free(arguments);
+    while (true) {
+        const bytes_read = std.os.linux.read(
+            std.os.linux.STDIN_FILENO,
+            &buffer,
+            BUFFER_SIZE,
+        );
+
+        if (bytes_read == 0) break; // EOF
+
+        if (bytes_read > BUFFER_SIZE) {
+            std.process.exit(1);
+        }
+
+        var total_written: usize = 0;
+        while (total_written < bytes_read) {
+            const bytes_written = std.os.linux.write(
+                std.os.linux.STDOUT_FILENO,
+                buffer[total_written..].ptr,
+                bytes_read - total_written,
+            );
+
+            if (bytes_written > (bytes_read - total_written)) {
+                std.process.exit(1);
+            }
+
+            total_written += bytes_written;
+        }
+    }
 }
